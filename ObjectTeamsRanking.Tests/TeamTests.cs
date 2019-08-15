b60d48d3c53cf5fbfc8ea94864faeb5026e8dc79
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-15 14:00:30 +0300

    Added some already passing tests - HasLessPointsWhenTeam1HasLessPointsThanTeam2ShouldReturnTrue - HasLessPointsWhenTeam1HasMorePointsThanTeam2ShouldReturnFalse - HasLessPointsWhenTeam1HasTheSamePointsThanTeam2ShouldReturnFalse - GetTeamDetailsWhenTeam1And12PointsShouldReturnArrayWithDetails

--- ObjectTeamsRanking.Tests/TeamTests.cs
+++ ObjectTeamsRanking.Tests/TeamTests.cs
@@ -6,9 +6,38 @@ namespace ObjectTeamsRanking.Tests
     public class TeamTests
     {
         [Fact]
-        public void Test1()
+        public void HasLessPointsWhenTeam1HasLessPointsThanTeam2ShouldReturnTrue()
         {
+            Team team1 = new Team("Team 1", 12);
+            Team team2 = new Team("Team 2", 24);
 
+            Assert.True(team1.HasLessPoints(team2));
+        }
+
+        [Fact]
+        public void HasLessPointsWhenTeam1HasMorePointsThanTeam2ShouldReturnFalse()
+        {
+            Team team1 = new Team("Team 1", 24);
+            Team team2 = new Team("Team 2", 12);
+
+            Assert.False(team1.HasLessPoints(team2));
+        }
+
+        [Fact]
+        public void HasLessPointsWhenTeam1HasTheSamePointsThanTeam2ShouldReturnFalse()
+        {
+            Team team1 = new Team("Team 1", 12);
+            Team team2 = new Team("Team 2", 12);
+
+            Assert.False(team1.HasLessPoints(team2));
+        }
+
+        [Fact]
+        public void GetTeamDetailsWhenTeam1And12PointsShouldReturnArrayWithDetails()
+        {
+            Team team1 = new Team("Team 1", 12);
+
+            Assert.Equal(new[] { "Team 1", "12" }, team1.GetTeamDetails());
         }
     }
 }
