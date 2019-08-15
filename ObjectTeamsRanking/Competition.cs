e32c169e3b5a2ed4ad5af9f24b0d085646a3d343
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-15 13:24:26 +0300

    Modified to meet the new requirements BEFORE - teams member initialized through method ReadTeams - BubbleSort method was public to allow sorting on demand - Teams clasification were printed to console through Print method - Swap method was printing the swappings done on the console - Teams details were introduced from console through ReadTeams method AFTER (still not implemented) - teams member initialized through AddTeam method - BubbleSort method will be private and used automatically after each call to AddTeam or AddResult - Teams clasification will be returned as a two dimensional string array - Swap method will just do the swapping without printing any details to the console - Team details will be added through method AddTeam and AddResult

--- ObjectTeamsRanking/Competition.cs
+++ ObjectTeamsRanking/Competition.cs
@@ -8,10 +8,10 @@ namespace ObjectTeamsRanking
 
         public Competition(int numberOfTeams)
         {
-            this.teams = ReadTeams(numberOfTeams);
+            this.teams = new Team[numberOfTeams];
         }
 
-        public void BubbleSort()
+        private void BubbleSort()
         {
             bool isSorted = false;
 
@@ -30,27 +30,9 @@ namespace ObjectTeamsRanking
             }
         }
 
-        public void Print()
-        {
-            for (int i = 0; i < teams.Length; i++)
-            {
-                teams[i].PrintName();
-                Console.Write("- ");
-                teams[i].PrintPoints();
-                Console.WriteLine("");
-            }
-        }
-
         private void Swap(int firstIndex, int secondIndex)
         {
             (int minIndex, int maxIndex) = GetMinMaxIndex(firstIndex, secondIndex);
-            const string message = "Swapping elements with indexes ({0}, {1}) and values (";
-            Console.Write(string.Format(message, minIndex, maxIndex));
-            teams[minIndex].PrintName();
-            Console.Write(", ");
-            teams[maxIndex].PrintName();
-            Console.WriteLine(")");
-
             Team temp = teams[minIndex];
             teams[minIndex] = teams[maxIndex];
             teams[maxIndex] = temp;
@@ -64,20 +46,6 @@ namespace ObjectTeamsRanking
             }
 
             return (firstIndex, secondIndex);
-        }
-
-        private Team[] ReadTeams(int numberOfTeams)
-        {
-            Team[] result = new Team[numberOfTeams];
-
-            for (int i = 0; i < result.Length; i++)
-            {
-                string[] teamData = Console.ReadLine().Split('-');
-                int points = Convert.ToInt32(teamData[1]) + Convert.ToInt32(teamData[2]);
-                result[i] = new Team(teamData[0], points);
-            }
-
-            return result;
         }
     }
 }
